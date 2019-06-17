4ee5373c5828303bc8260b51acf508d0753f32af
Valentina Urakhchinskaya <valentina.urakhchinskaya@aexp.com>
2019-06-17 15:53:54 -0400

    Initial commit

--- FidelsGame/ViewController.swift
+++ FidelsGame/ViewController.swift
@@ -10,11 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func sayRrrMeow(_ sender: UIButton) {
+    print("Fidel says Rrr-Meow!")
     }
-
-
+    
 }
 
