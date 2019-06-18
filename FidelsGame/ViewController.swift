b38bbae6661bda58e034f27b90bb1e1e65ecabc2
Valentina Urakhchinskaya <valentina.urakhchinskaya@aexp.com>
2019-06-18 05:15:45 -0400

    Movement animation

--- FidelsGame/ViewController.swift
+++ FidelsGame/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var animationIsActive = false
 
     @IBAction func sayRrrMeow(_ sender: UIButton) {
     print("Fidel says Rrr-Meow!")
+    
+        if(!animationIsActive){
+            animationIsActive = true
+            fidelsAnimation(sender: sender)
+        } else {
+            animationIsActive = false
+        }
+        
     }
+    
+    func fidelsAnimation(sender: UIButton){
+        if(animationIsActive){
+        [UIView.animate(withDuration: 0.5, delay: 0.0, options: [.allowUserInteraction], animations: {
+            
+            let deltaX = CGFloat(Int.random(in: Int(-1 * sender.center.x) ... Int(UIScreen.main.bounds.width -  sender.center.x)))
+            let deltaY = CGFloat(Int.random(in: Int(-1 * sender.center.y) ... Int(UIScreen.main.bounds.height - sender.center.y)))
+            
+            sender.frame = CGRect(x: sender.frame.origin.x + deltaX, y: sender.frame.origin.y + deltaY, width: sender.frame.width, height: sender.frame.height)
+            
+            
+        }, completion: { _ in
+            self.fidelsAnimation(sender: sender)
+        })]
+        }
+    }
+    
     
 }
 
