08c374e5b20133a840f12e785809b91627740faf
Valentina Urakhchinskaya <valentina.urakhchinskaya@aexp.com>
2019-06-19 02:46:15 -0400

    Home screen initial commit

---
+++ FidelsGame/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  FidelsGame
+//
+//  Created by Valentina Urakhchinskaya on 6/19/19.
+//  Copyright © 2019 Valentina Urakhchinskaya. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var SpeedPickerView: UIPickerView!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
