c931604c2c74a7fa5d95fc5a0f15d82c7e6243f8
Valentina Urakhchinskaya <valentina.urakhchinskaya@aexp.com>
2019-06-20 10:45:32 -0400

    - Game rules adjustment - Navigation bar titles

--- FidelsGame/ViewController.swift
+++ FidelsGame/ViewController.swift
@@ -10,34 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var animationIsActive = false
+    var tapCount = 0
     
     var speed = 0.0
-
-    @IBAction func sayRrrMeow(_ sender: UIButton) {
-    print("Fidel says Rrr-Meow!")
     
-        if(!animationIsActive){
-            animationIsActive = true
-            fidelsAnimation(sender: sender)
-        } else {
-            animationIsActive = false
-        }
-        
+    var animationIsActive = true
+    
+    @IBOutlet weak var buttonStopStart: UIBarButtonItem!
+    
+    @IBOutlet weak var animationButton: UIButton!
+    
+    override func viewDidLoad(){
+        fidelsAnimation(animationButton: animationButton)
+    }
+    @IBAction func sayRrrMeow() {
+        tapCount+=1
+        print(tapCount)
     }
     
-    func fidelsAnimation(sender: UIButton){
+    @IBAction func gameStopStart(_ sender: UIBarButtonItem) {
+        if(animationIsActive){
+            //TODO: display results
+            animationIsActive = false
+            buttonStopStart.title = "Start"
+            animationButton.isEnabled = false
+        }else {
+            animationIsActive = true
+            buttonStopStart.title = "Stop"
+            animationButton.isEnabled = true
+            tapCount = 0
+            fidelsAnimation(animationButton: animationButton)
+        }
+    }
+    
+    func fidelsAnimation(animationButton: UIButton){
         if(animationIsActive){
         UIView.animate(withDuration: speed, delay: 0.0, options: [.allowUserInteraction], animations: {
             
-            let deltaX = CGFloat(Int.random(in: Int(-1 * sender.center.x) ... Int(UIScreen.main.bounds.width -  sender.center.x)))
-            let deltaY = CGFloat(Int.random(in: Int(-1 * sender.center.y) ... Int(UIScreen.main.bounds.height - sender.center.y)))
+            let deltaX = CGFloat(Int.random(in: Int(-1 * animationButton.center.x) ... Int(UIScreen.main.bounds.width -  animationButton.center.x)))
+            let deltaY = CGFloat(Int.random(in: Int(-1 * animationButton.center.y) ... Int(UIScreen.main.bounds.height - animationButton.center.y)))
             
-            sender.frame = CGRect(x: sender.frame.origin.x + deltaX, y: sender.frame.origin.y + deltaY, width: sender.frame.width, height: sender.frame.height)
+            animationButton.frame = CGRect(x: animationButton.frame.origin.x + deltaX, y: animationButton.frame.origin.y + deltaY, width: animationButton.frame.width, height: animationButton.frame.height)
             
             
         }, completion: { _ in
-            self.fidelsAnimation(sender: sender)
+            self.fidelsAnimation(animationButton: animationButton)
         })
         }
     }
